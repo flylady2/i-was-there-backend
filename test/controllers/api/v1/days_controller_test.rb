c45519134219acbf1e91a714c208233bc18873ef
Leslie Stevens <flymaven@protonmail.com>
2020-12-18 16:33:04 -0600

    code for tests

--- test/controllers/api/v1/days_controller_test.rb
+++ test/controllers/api/v1/days_controller_test.rb
@@ -1,6 +1,28 @@
 require 'test_helper'
 
 class Api::V1::DaysControllerTest < ActionDispatch::IntegrationTest
+
+  test "should create day" do
+    assert_difference('Day.count') do
+      post api_v1_days_url, params: { day: {:name => "Tuesday", :date => "2020-12-15"}}
+      
+    end
+  end
+  #  end
+  #end
+  test "should get index" do
+    get api_v1_days_url
+    assert_response :success
+  end
+
+  test "should destroy day" do
+    @day = days(:one)
+    assert_difference("Day.count", -1) do
+      delete api_v1_day_url(@day)
+    end
+  end
+
+
   # test "the truth" do
   #   assert true
   # end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
+
+  test "should not save entry without content" do
+    entry = Entry.new
+    assert_not entry.save
+  end
+
+  test "should not save entry without day_id" do
+    entry = Entry.new
+    assert_not entry.save
+  end
+
+  test "should not save entry without category_id" do
+    entry = Entry.new
+    assert_not entry.save
+  end
+
+
   # test "the truth" do
   #   assert true
   # end
