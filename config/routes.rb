6d78623351458bb13d8955b333c42ccf92caf481
Leslie Stevens <flymaven@protonmail.com>
2021-06-01 19:28:16 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :users, only: [:index, :create,]
+      resources :users, only: [:index, :create]
       post '/login', to: 'auth#create'
       #get '/newUserLogin', to: 'auth#newUserLogin'
       post '/signup', to: 'users#create'
